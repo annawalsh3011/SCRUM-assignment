58627bea27a60e55ed9589f355b86243a2b62c45
jack-kennedy56 <78079786+jack-kennedy56@users.noreply.github.com>
2021-11-16 11:52:57 +0000

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,21 @@
+choice = 0
+
+print('welcome')
+
+while choice != '5':
+    print('1: Customers')
+    print('2: Suppliers')
+    print('3: Stock')
+    print('4: Oders')
+    print('5: Exit')
+
+    choice = input('Choose the number of the function you wish to use: ')
+
+    if choice == '1':
+       print('A')
+    if choice == '2':
+        print('A')
+    if choice == '3':
+        print('A')
+    if choice == '4':
+        print('A')
