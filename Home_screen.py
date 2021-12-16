4f17450a2a03f7fae3598c4c6289ef47ebe33a9a
annawalsh3011 <94450965+annawalsh3011@users.noreply.github.com>
2021-12-16 13:21:11 +0000

    Add files via upload

--- Home_screen.py
+++ Home_screen.py
@@ -1,58 +1,103 @@
 from Item1 import Item
+from Item1 import add_basket
+from Item1 import sum_basket
+from Item1 import change_calculator
 import datetime
 
-choice = 0
+
+
 
 print('Welcome')
 print('Below is a menu')
+a_choice=0 
+while a_choice != '3':
+    a_choice = input('Enter 1 if you are a  staff member or 2 if you are a customer:  ')
 
-while choice != '4':
-    print('1: Add Items')
-    print('2: View all items')
-    print('3. Delete expired items')
-    print('4: Exit')
+    if a_choice == '1':
+        choice = 0
+        while choice != '4':
+            print('1: Add Items')
+            print('2: View all items')
+            print('3. Delete expired items')
+            print('4: Exit')
 
-    choice = input('Choose the number of the function you wish to use: ')
+            choice = input('Choose the number of the function you wish to use: ')
 
-    if choice == '1':
-        #add items
-        name = input('Enter item name: ')
-        type = input('Enter the number of the item type from: 1 = luxury, 2 = essential or 3 = gift ')
-        amount = input('Enter the amount of stock you want to add')
-       # expiration_date = input('Enter expiration date of product in format of dd/mm/yyyy (10/12/2018): ')
-       
-        print('does this item have an expiration date?')
-        e_d= input('Enter 1 for yes or 2 for no')
-        if e_d == '1':
-            day= int(input('enter the date of expiry .i.e enter 3 if it is the 3rd  '))
-            month= int(input('enter the month of expiry in numbers i.e enter 6 if it is june  '))
-            year = int(input('enter the yeaar of expiry i.e. 2008  '))
-            expiration_date= datetime.date(year, month, day)
+            if choice == '1':
+                #add items
+                name = input('Enter item name: ')
+                type = input('Enter the number of the item type from: 1 = luxury, 2 = essential or 3 = gift ')
+                amount = input('Enter the amount of stock you want to add')
+            # expiration_date = input('Enter expiration date of product in format of dd/mm/yyyy (10/12/2018): ')
+            
+                print('does this item have an expiration date?')
+                e_d= input('Enter 1 for yes or 2 for no')
+                if e_d == '1':
+                    day= int(input('enter the date of expiry .i.e enter 3 if it is the 3rd  '))
+                    month= int(input('enter the month of expiry in numbers i.e enter 6 if it is june  '))
+                    year = int(input('enter the yeaar of expiry i.e. 2008  '))
+                    expiration_date= datetime.date(year, month, day)
 
-            if name and type and amount and expiration_date != '':
-                Item.add_item( name, type, expiration_date, amount) 
-            else:
-                print('item not added as not all fields were incomplete')
-        else:
-            if name and type and amount != '':
-                expiration_date=''
-                Item.add_item( name, type, expiration_date, amount)
-            else:
-                print('item not added as not all fields were incomplete')
+                    if name and type and amount and expiration_date != '':
+                        Item.add_item( name, type, expiration_date, amount) 
+                    else:
+                        print('item not added as not all fields were incomplete')
+                else:
+                    if name and type and amount != '':
+                        expiration_date=''
+                        Item.add_item( name, type, expiration_date, amount)
+                    else:
+                        print('item not added as not all fields were incomplete')
 
-        
-    if choice == '2':
-        all = Item.view_all_items()
+                
+            if choice == '2':
+                all = Item.view_all_items()
 
-        delete_choice =''
-        print('Do you want to delete an item from the list?')
-        delete_choice= input('Enter 1 for Yes, enter 2 to return to home screen ')
-        if delete_choice== '1':
-            del_item= input('enter the name of item you want to be deleted')
-            Item.delete(del_item)
-        else:
-            print('exit')
-            ##### return back to home screen
-    
-    if choice == '3':
-        Item.delete_expired_items()
+                delete_choice =''
+                print('Do you want to delete an item from the list?')
+                delete_choice= input('Enter 1 for Yes, enter 2 to return to home screen ')
+                if delete_choice== '1':
+                    del_item= input('enter the name of item you want to be deleted')
+                    Item.delete(del_item)
+                else:
+                    print('exit')
+                    ##### return back to home screen
+            
+            if choice == '3':
+                Item.delete_expired_items()
+
+            if choice == '4':
+                a_choice = '0'
+
+    if a_choice == '2':
+        choice_2 = 0
+        while choice_2 != '4':
+            print('1: view all items')
+            print('2: Purchase items')
+            print('3. Calculate change')
+            print('4: Exit')
+
+            choice_2 = input('Choose the number of the function you wish to use: ')
+            
+
+            if choice_2 =='1':
+                all = Item.view_all_items()
+
+            if choice_2 == '2':
+                option = int(input('Enter the number of item you want to purchase: '))
+                for i in range(option):
+                    name= input('enter the exact name of the product from the list above that you wish to purchase')
+                    quantity = int(input('enter the amount of items you wish to purchase'))
+                    add_basket(name, quantity)
+
+                    res = sum_basket(name, quantity)
+                print(res)
+            
+            if choice_2 == '3':
+                amount_due = res
+                amount_due = float(amount_due)
+                print(f'The amount dew is {amount_due}')
+                amount_pay = float(input('Enter the amount you want to pay: '))
+                change = change_calculator(amount_due, amount_pay)
+
+
--- Item1.py
+++ Item1.py
@@ -4,6 +4,7 @@ import datetime
 
 stock_items=[]
 expired_items=[]
+basket_list=[]
 
 class Item():
     def __init__(self):
@@ -84,7 +85,7 @@ class Item():
         if type == '2':
             i.set_type('Essential')
         if type == '3':
-            i.set_type('Luxury')
+            i.set_type('Gift')
         
         i.set_expiration_date(expiration_date)
         
@@ -119,4 +120,89 @@ class Item():
                 expired_items.append(stock_items[i-1])
 
 
+def add_basket(name, q):
+    for i in range(len(stock_items)):
+        if stock_items[i-1].get_name() == name:
+            a  = stock_items[i-1]
+            a.set_quantity(q)
+            basket_list.append(a)
+    
+    for b in basket_list:
+        print(b)
+
+def sum_basket(name, q):
+    L = 'Luxury'
+    G= 'Gift'
+    E= 'Essential'
+    count = 0
+    q = int(q)
+    for i in range(len(stock_items)):
+        if stock_items[i-1].get_name() == name:
+            for b in range(len(basket_list)): 
+                if basket_list[b-1].get_type() == L:
+                    vat = q * 50 * .2
+                    vat = int(vat)
+                    count = int(count)
+                    count = count + (50 * q) + vat
+                if basket_list[b-1].get_type() == G:
+                    vat = q * 20 * .05
+                    vat = int(vat)
+                    count = int(count)
+                    count = count + (20 * q) + vat
+                if basket_list[b-1].get_type() == E:
+                    vat = q * 30 * .1
+                    vat = int(vat)
+                    count = int(count)
+                    count = count + (30 * q) + vat
+
                 
+            return count  
+
+
+
+def change_calculator(total_cost, amount_paid):
+    total_change= amount_paid- total_cost
+    total_change = round(total_change, 2)
+    print(f"Total_change: {total_change}")
+
+    hundred_euros = int(total_change / 100)   # the number of hundred euros
+    new_balance=total_change-(hundred_euros*100) # getting the remainder
+    fifty_euros = int(new_balance/50)
+    new_balance=new_balance-(fifty_euros*50)
+    twenty_euros = int(new_balance/20)
+    new_balance=new_balance-(twenty_euros*20)
+    ten_euros = int(new_balance/10)
+    new_balance=new_balance-(ten_euros*10)
+    five_euros = int(new_balance/5)
+    new_balance=new_balance-(five_euros*5)
+    two_euros = int(new_balance/2)
+    new_balance=new_balance-(two_euros*2)
+    one_euros = int(new_balance/1)
+    new_balance=new_balance-(one_euros*1)
+    fifty_cent= int(new_balance/(0.5))
+    new_balance= new_balance-(fifty_cent*(0.5))
+    twenty_cent= int(new_balance/(0.2))
+    new_balance= new_balance-(twenty_cent*(0.2))
+    ten_cent= int(new_balance/(0.1))
+    new_balance=new_balance-(ten_cent*(0.1))
+    five_cent= int(new_balance/(0.05))
+    new_balance=new_balance-(five_cent*(0.05))
+    two_cent= int(new_balance/(0.02))
+    new_balance=new_balance-(two_cent*(0.02))
+    one_cent=int(new_balance/(0.01)) 
+
+    Change=(f'{hundred_euros}-  €100 note, {fifty_euros}- €50 note, {twenty_euros}- €20 note, {ten_euros}- €10 note , {five_euros}- €5 note, {two_euros}- €2 coin, {one_euros}- €1 coin, {fifty_cent}- 50 cent coin, {twenty_cent}- 20 cent coin,  {ten_cent} - 10 cent coin,  {five_cent} - 5 cent coin,  {two_cent} - 2 cent coin,  {one_cent} - 1 cent coin')
+    print(Change)
+
+    print("do you want to clear everything?")
+    print("1. Yes")
+    print("2. No")
+    option= input("enter either 1 or 2")
+    if option=='1':
+        total_cost=0
+        total_change=0
+    else:
+        print('it is not cleared')
+    return Change
+    
+
